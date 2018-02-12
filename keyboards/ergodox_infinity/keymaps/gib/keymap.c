ae1a207d2a5f4f0389a258e73de67fdfaee3923e
Gibson Fahnestock <gibfahn@gmail.com>
2018-02-12 12:35:37 +0000

    Improve layout and docs

--- keyboards/ergodox_infinity/keymaps/gib/keymap.c
+++ keyboards/ergodox_infinity/keymaps/gib/keymap.c
@@ -57,37 +57,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
     // left hand
-    RESET          , KC_1        , KC_2    , KC_3       , KC_4       , KC_5          , TG(LYR2)   ,
-    KC_TAB         , KC_Q        , KC_W    , KC_E       , KC_R       , KC_T          , RSFT(KC_9) ,
-    KC_RSFT        , KC_A        , KC_S    , KC_D       , KC_F       , KC_G          ,
-    LCTL_T(KC_ESC) , CTL_T(KC_Z) , KC_X    , KC_C       , KC_V       , KC_B          , KC_LBRC    ,
-    KC_F17         , _______     , KC_LALT , KC_LCTL    , KC_LGUI
+    RESET          , KC_1        , KC_2    , KC_3    , KC_4    , KC_5     , TG(LYR2)   ,
+    KC_TAB         , KC_Q        , KC_W    , KC_E    , KC_R    , KC_T     , RSFT(KC_9) ,
+    KC_RSFT        , KC_A        , KC_S    , KC_D    , KC_F    , KC_G     ,
+    LCTL_T(KC_ESC) , CTL_T(KC_Z) , KC_X    , KC_C    , KC_V    , KC_B     , KC_LBRC    ,
+    KC_F17         , _______     , KC_LALT , KC_LCTL , KC_LGUI
 
-    /* _______     , _______     , _______ , _______    , _______ */ , KC_F17        , _______
-    /* _______     , _______     , _______ , _______    , _______    , _______ */    , KC_LALT
-    /* _______     , _______     , _______ , _______ */ , KC_RSFT    , MO(LYR1)      , KC_LGUI    ,
+    /*_______      , _______     , _______ , _______ , _____*/ , KC_F17   , _______
+    /*_______      , _______     , _______ , _______ , _______ , _____*/  , KC_LALT
+    /*_______      , _______     , _______ , _____*/ , KC_RSFT , MO(LYR1) , KC_LGUI    ,
 
     // right hand
-    TG(LYR2)      , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , RESET   ,
-    RSFT(KC_0)    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_MINS
-                  , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
-    KC_RBRC       , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_BSLS
-                  , KC_RSFT , KC_RALT , _______ , _______ , KC_FN1  ,
+    TG(LYR2)       , KC_6        , KC_7    , KC_8    , KC_9    , KC_0     , RESET      ,
+    RSFT(KC_0)     , KC_Y        , KC_U    , KC_I    , KC_O    , KC_P     , KC_MINS
+                   , KC_H        , KC_J    , KC_K    , KC_L    , KC_SCLN  , KC_QUOT    ,
+    KC_RBRC        , KC_N        , KC_M    , KC_COMM , KC_DOT  , KC_SLSH  , KC_BSLS
+                   , KC_RSFT     , KC_RALT , _______ , _______ , KC_FN1   ,
 
-    _______       , _______ ,
-    KC_LALT       ,
-    KC_LCTL       , KC_BSPC , KC_SPC
+    _______        , _______     ,
+    KC_LALT        ,
+    KC_LCTL        , KC_BSPC     , KC_SPC
     ),
-/* Keymap 1: Hyper Layer
+/* Keymap 1: Movement Layer
+ * Anything blank (KC_TRNS) does whatever layer below did.
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |      |   7  |   8  |   9  |      |      |           |      |  ↖   |  ⇟   |  ⇞   |  ↘   |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   0  |   4  |   5  |   6  |      |------|           |------|  ◀   |  ▼   |  ▲   |  ▶   |  =   |   `    |
+ * |         |   0  |   4  |   5  |   6  |  .   |------|           |------|  ◀   |  ▼   |  ▲   |  ▶   |  =   |   `    |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   0  |   1  |   2  |   3  |      |      |           |      |      |      |      |      |      |        |
+ * |         |   0  |   1  |   2  |   3  |  .   |      |           |      | 🐭 ◀ | 🐭▼  | 🐭▲  | 🐭 ▶ |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | EPRM  |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -102,28 +103,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Layer 1
 [LYR1] = LAYOUT_ergodox(
     // left hand
-    VRSN    , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_TRNS ,
-    KC_TRNS , KC_EXLM , KC_7    , KC_8    , KC_9    , KC_PIPE , KC_TRNS ,
+    VRSN          , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_TRNS ,
+    KC_TRNS       , KC_EXLM , KC_7    , KC_8    , KC_9    , KC_PIPE , KC_TRNS ,
 
-    KC_TRNS , KC_0    , KC_4    , KC_5    , KC_6    , KC_GRV  ,
-    KC_TRNS , KC_0    , KC_1    , KC_2    , KC_3    , KC_TILD , KC_TRNS ,
-       EPRM , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
+    KC_TRNS       , KC_0    , KC_4    , KC_5    , KC_6    , KC_DOT  ,
+    KC_TRNS       , KC_0    , KC_1    , KC_2    , KC_3    , KC_DOT  , KC_TRNS ,
+       EPRM       , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
 
-     /* _______ , _______ , _______ , _______    , _______ */ , KC_TRNS         , KC_TRNS
-     /* _______ , _______ , _______ , _______    , _______    , _______      */ , KC_TRNS
-     /* _______ , _______ , _______ , _______ */ , KC_TRNS    , KC_TRNS         , KC_TRNS ,
+     /* _______   , _______ , _______ , _______ , _____*/ , KC_TRNS , KC_TRNS
+     /* _______   , _______ , _______ , _______ , _______ , ____*/  , KC_TRNS
+     /* _______   , _______ , _______ , _____*/ , KC_TRNS , KC_TRNS , KC_TRNS ,
 
 
     // right hand
-    KC_TRNS   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
-    KC_TRNS   , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_ASTR , KC_F12
-              , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_EQL  , KC_GRV  ,
-    KC_TRNS   , KC_AMPR , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
-              , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
+    KC_TRNS       , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  ,
+    KC_TRNS       , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_ASTR , KC_F12
+                  , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_EQL  , KC_GRV  ,
+    KC_TRNS       , KC_AMPR , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
+                  , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
 
-    RGB_TOG   , RGB_SLD ,
-    KC_TRNS   ,
-    KC_TRNS   , KC_DEL  , KC_ENT
+    RGB_TOG       , RGB_SLD ,
+    KC_TRNS       ,
+    KC_TRNS       , KC_DEL  , KC_ENT
 
 ),
 /* Keymap 2: Media and mouse keys
